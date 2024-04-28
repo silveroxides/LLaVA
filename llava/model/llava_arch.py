5e6c92033f3ef1304d0d956028ac9ea99dceddcb
20-42619-1@student.aiub.edu <20-42619-1@student.aiub.edu>
2024-04-28 07:43:36 +0600

    moe-forward-function-called

--- llava/model/llava_arch.py
+++ llava/model/llava_arch.py
@@ -139,7 +139,10 @@ class LlavaMetaForCausalLM(ABC):
 
     def encode_images(self, images):
         image_features = self.get_model().get_vision_tower()(images)
-        image_features = self.get_model().mm_projector(image_features)
+        # Assuming self.get_model().mm_projector is an instance of SparseMoeBlock
+        moe_block = self.get_model().mm_projector
+        image_features = moe_block.forward(image_features)
+
         return image_features
 
     def prepare_inputs_labels_for_multimodal(
