6a65954724dd601ed558a50732d37cdeacafef1a
20-42619-1@student.aiub.edu <20-42619-1@student.aiub.edu>
2024-05-02 19:48:11 +0600

    tracking-multimedia-projector-builder.py

--- llava/train/train.py
+++ llava/train/train.py
@@ -829,11 +829,9 @@ def train(attn_implementation=None):
             #     del bnb_model_from_pretrained_args['low_cpu_mem_usage']
             # if 'device_map' in bnb_model_from_pretrained_args:
             #     del bnb_model_from_pretrained_args['device_map']
-            config = transformers.AutoConfig.from_pretrained(model_args.model_name_or_path, trust_remote_code=True)
             model = LlavaLlamaForCausalLM.from_pretrained(
                 model_args.model_name_or_path,
                 cache_dir=training_args.cache_dir,
-                config=config,
                 attn_implementation=attn_implementation,
                 torch_dtype=(torch.bfloat16 if training_args.bf16 else None),
                 **bnb_model_from_pretrained_args
