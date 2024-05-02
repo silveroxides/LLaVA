069647519a775d21b727bd0daa02864dd070616a
20-42619-1@student.aiub.edu <20-42619-1@student.aiub.edu>
2024-05-03 00:17:48 +0600

    debugging-config

--- llava/train/train.py
+++ llava/train/train.py
@@ -830,7 +830,11 @@ def train(attn_implementation=None):
             #     del bnb_model_from_pretrained_args['low_cpu_mem_usage']
             # if 'device_map' in bnb_model_from_pretrained_args:
             #     del bnb_model_from_pretrained_args['device_map']
-            # config = AutoConfig.from_pretrained(model_args.model_name_or_path, trust_remote_code=True)
+            config = AutoConfig.from_pretrained(model_args.model_name_or_path, trust_remote_code=True)
+            print('##################################################--AutoConfig--###############################################')
+            print(config)
+            print('###############################################################################################################') 
+
             model = LlavaLlamaForCausalLM.from_pretrained(
                 model_args.model_name_or_path,
                 cache_dir=training_args.cache_dir,
