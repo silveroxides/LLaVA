1f4604dd4d7ea806a8715d312f4f6f379609ac5c
20-42619-1@student.aiub.edu <20-42619-1@student.aiub.edu>
2024-05-04 19:51:20 +0600

    debug moe implementation

--- llava/train/train.py
+++ llava/train/train.py
@@ -837,7 +837,7 @@ def train(attn_implementation=None):
                 cache_dir=training_args.cache_dir,
                 attn_implementation=attn_implementation,
                 torch_dtype=(torch.bfloat16 if training_args.bf16 else None),
-                mm_projector_type=model_args.mm_projector_type,
+                model_args=model_args,  # Pass model_args 
                 **bnb_model_from_pretrained_args
             )
             print('##############################model = LlavaLlamaForCausalLM.from_pretrained#########################')
