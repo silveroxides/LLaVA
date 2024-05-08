9d423cca85ddc71ee761ea50f166da7d707cae7f
20-42619-1@student.aiub.edu <20-42619-1@student.aiub.edu>
2024-05-08 10:13:41 +0600

    wandb initialization fixed

--- myProject/setup_env.sh
+++ myProject/setup_env.sh
@@ -4,8 +4,13 @@
 # Store the current directory
 previous_dir=$(pwd)
 
-# Go up one directory to the main directory
-
+# Create conda environment and install packages
+conda create -n llava python=3.10 -y
+conda activate llava
+pip install --upgrade pip
+pip install -e .
+pip install -e ".[train]"
+pip install flash-attn --no-build-isolation
 
 # Set up environment variables using Python
 python -c '
@@ -14,13 +19,5 @@ import wandb
 wandb.login(key="a9cd634aa6e8ac711e4a18b073f238ff96672289")
 os.environ["WANDB_PROJECT"]="FineTuneLLaVa"
 '
-
-# Create conda environment and install packages
-conda create -n llava python=3.10 -y
-conda activate llava
-pip install --upgrade pip
-pip install -e .
-pip install -e ".[train]"
-pip install flash-attn --no-build-isolation
 
 
