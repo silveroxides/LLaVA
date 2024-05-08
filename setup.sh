ea0ffa5169f6b8239e5b4191a98a0a5adbccd2bc
20-42619-1@student.aiub.edu <20-42619-1@student.aiub.edu>
2024-05-08 09:28:55 +0600

    setup.sh-file-added

---
+++ setup.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Store the current directory
+previous_dir=$(pwd)
+
+# Create a folder
+folder_name="dataset"
+mkdir -p "$folder_name"
+echo "Folder $folder_name created successfully."
+
+# Change directory to the dataset folder
+cd "$folder_name"
+
+# Download files
+wget https://huggingface.co/datasets/liuhaotian/LLaVA-CC3M-Pretrain-595K/resolve/main/chat.json
+wget https://huggingface.co/datasets/liuhaotian/LLaVA-CC3M-Pretrain-595K/resolve/main/images.zip
+
+# Unzip the files
+unzip -q images.zip
+
+# Delete unnecessary zip file
+rm -f images.zip
+echo "File images.zip deleted successfully."
+
+# Go back to the previous directory
+cd "$previous_dir"
