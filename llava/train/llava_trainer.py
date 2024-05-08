cc8af10c19136eccfb8c93b82575ec63b45bb9ba
20-42619-1@student.aiub.edu <20-42619-1@student.aiub.edu>
2024-05-08 22:30:38 +0600

    debug the new auxilary loss issues

--- llava/train/llava_trainer.py
+++ llava/train/llava_trainer.py
@@ -179,7 +179,7 @@ class LLaVATrainer(Trainer):
         print(f'Main Loss: {loss}')
 
         # Add the aux_loss to the main loss
-        combined_loss = loss + load_balancing_loss.detouch()
+        combined_loss = loss + load_balancing_loss.detach()
 
         print(f'Total Loss: {combined_loss}')
         print('-'*100)
