9518a024606c9426ff1706316e797108f340e791
20-42619-1@student.aiub.edu <20-42619-1@student.aiub.edu>
2024-05-09 10:41:50 +0600

    flash-attention-2 reverted

--- llava/train/train_mem.py
+++ llava/train/train_mem.py
@@ -4,4 +4,4 @@ from llava.train.train import train
 # or being imported as a module into another script.
 # directly run in using the deepspeed command line
 if __name__ == "__main__":
-    train(attn_implementation="eager")
+    train(attn_implementation="flash_attention_2")
