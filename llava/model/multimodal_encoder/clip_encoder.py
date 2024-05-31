9cf034d0896d579e7fc7fe7bb43e63aadeba3936
20-42619-1@student.aiub.edu <20-42619-1@student.aiub.edu>
2024-06-01 00:34:14 +0600

    debugging image processing through moe

--- llava/model/multimodal_encoder/clip_encoder.py
+++ llava/model/multimodal_encoder/clip_encoder.py
@@ -56,9 +56,11 @@ class CLIPVisionTower(nn.Module):
                 image_feature, router_logits = self.feature_select(image_forward_out).to(image.dtype)
                 image_features.append(image_feature)
                 router_logits.append(router_logits)
+        
+        # image is not a list but tensor
         else:
-            image_forward_outs = self.vision_tower(images.to(device=self.device, dtype=self.dtype), output_hidden_states=True)
-            image_feature, router_logits = self.feature_select(image_forward_outs).to(images.dtype)
+            image_forward_outs, router_logits = self.vision_tower(images.to(device=self.device, dtype=self.dtype), output_hidden_states=True)
+            image_feature = self.feature_select(image_forward_outs).to(images.dtype)
 
         return image_features
 
