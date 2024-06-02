eb31f8c63aa3b0451d59adb4a54b3b1f5125a411
20-42619-1@student.aiub.edu <20-42619-1@student.aiub.edu>
2024-06-02 21:17:44 +0600

    image feature select method modified

--- llava/model/multimodal_encoder/clip_encoder.py
+++ llava/model/multimodal_encoder/clip_encoder.py
@@ -44,8 +44,9 @@ class CLIPVisionTower(nn.Module):
 
         self.is_loaded = True
 
-    def feature_select(self, image_forward_outs):
-        image_features = image_forward_outs.hidden_states[self.select_layer]
+    def feature_select(self, image_features):
+        # image_features = image_forward_outs.hidden_states[self.select_layer]
+
         if self.select_feature == 'patch':
             image_features = image_features[:, 1:]
         elif self.select_feature == 'cls_patch':
