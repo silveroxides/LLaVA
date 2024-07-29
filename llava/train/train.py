bb95ca36d1247bbd1e69cfbcc7822e3d5ead93ad
20-42619-1@student.aiub.edu <20-42619-1@student.aiub.edu>
2024-07-29 06:45:47 +0600

    solving: upscaling fp16 error

--- llava/train/train.py
+++ llava/train/train.py
@@ -877,7 +877,7 @@ def train(attn_implementation=None):
             model_args.model_name_or_path,
             cache_dir=training_args.cache_dir,
             attn_implementation=attn_implementation,
-            torch_dtype=(torch.bfloat16 if training_args.bf16 else torch.fp32),
+            torch_dtype=(torch.bfloat16 if training_args.bf16 else torch.float32),
             **bnb_model_from_pretrained_args
         )
 
