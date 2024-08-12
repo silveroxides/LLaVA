a5c0773407c926d63e4935b0001297de841e8ddb
20-42619-1@student.aiub.edu <20-42619-1@student.aiub.edu>
2024-08-12 22:05:56 +0600

    pretrain bash command chnaged

--- myProject/pretrain.sh
+++ myProject/pretrain.sh
@@ -11,28 +11,33 @@ deepspeed llava/train/train_mem.py \
     --mm_vision_select_layer -2 \
     --mm_use_im_start_end False \
     --mm_use_im_patch_token False \
-    --fp16 True \
-    --output_dir ./checkpoints/llava-vicuna-v5-3-7b \
-    --max_steps 50 \
+    --bf16 True \
+    --output_dir ./ckpts/llava-vicuna-v2-3-7b \
+    --num_train_epochs 1 \
     --num_experts_per_tok 2 \
     --num_experts 4 \
+    --num_layers 4 \
+    --num_heads 4 \
     --aux_loss_coef 0.01 \
-    --mm_projector_type 'sparse_moe'\
-    --per_device_train_batch_size 4 \
+    --share_moe False \
+    --cross_attension True \
+    --mm_projector_type 'mlp2x_gelu'\
+    --per_device_train_batch_size 32 \
     --per_device_eval_batch_size 4 \
-    --gradient_accumulation_steps 4 \
+    --gradient_accumulation_steps 1 \
     --evaluation_strategy "no" \
     --save_strategy "steps" \
-    --save_steps 100 \
+    --save_steps 24000 \
     --save_total_limit 1 \
     --learning_rate 2e-3 \
     --weight_decay 0. \
     --warmup_ratio 0.03 \
     --lr_scheduler_type "cosine" \
     --logging_steps 1 \
-    --tf32 False \
+    --tf32 True \
     --model_max_length 2048 \
     --gradient_checkpointing True \
     --dataloader_num_workers 4 \
     --lazy_preprocess True \
     --report_to wandb \
+    --run_name "UVA-Pretrain-mlp"
