b261752135458da2f4e77bc47c2d2af3320adf9c
20-42619-1@student.aiub.edu <20-42619-1@student.aiub.edu>
2024-08-16 18:18:11 +0600

    script for pretrain dataset added

--- myProject/setup.sh
+++ myProject/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Create a folder
-folder_name="dataset"
+folder_name="./playground/data/LLaVA-Pretrain"
 mkdir -p "$folder_name"
 echo "Folder $folder_name created successfully."
 
@@ -9,11 +9,22 @@ echo "Folder $folder_name created successfully."
 cd "$folder_name"
 
 # Download files
-wget https://huggingface.co/datasets/liuhaotian/LLaVA-CC3M-Pretrain-595K/resolve/main/chat.json
-wget https://huggingface.co/datasets/liuhaotian/LLaVA-CC3M-Pretrain-595K/resolve/main/images.zip
+wget https://huggingface.co/datasets/liuhaotian/LLaVA-Pretrain/resolve/main/blip_laion_cc_sbu_558k.json
+
+# Go to the previous directory
+cd ..
+
+# Create a folder named images
+mkdir -p images
+
+# Change directory to the images folder
+cd images
+
+wget https://huggingface.co/datasets/liuhaotian/LLaVA-Pretrain/resolve/main/images.zip
 # Unzip the files
 unzip -q images.zip
 
 # Delete unnecessary zip file
 rm -f images.zip
+
 echo "File images.zip deleted successfully."
--- myProject/setup_env.sh
+++ myProject/setup_env.sh
@@ -11,13 +11,3 @@ pip install --upgrade pip
 pip install -e .
 pip install -e ".[train]"
 pip install flash-attn --no-build-isolation
-
-# Set up environment variables using Python
-python -c '
-import os
-import wandb
-wandb.login(key="a9cd634aa6e8ac711e4a18b073f238ff96672289")
-os.environ["WANDB_PROJECT"]="FineTuneLLaVa"
-'
-
-
