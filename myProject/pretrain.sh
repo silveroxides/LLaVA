cfdfd547c1db063053d4b55c5fefb658cbd34a60
20-42619-1@student.aiub.edu <20-42619-1@student.aiub.edu>
2024-08-17 20:02:49 +0600

    test_slurm_script

--- myProject/pretrain.sh
+++ myProject/pretrain.sh
@@ -4,8 +4,8 @@ deepspeed llava/train/train_mem.py \
     --deepspeed ./scripts/zero2.json \
     --model_name_or_path lmsys/vicuna-13b-v1.5 \
     --version plain \
-    --data_path ./dataset/chat.json \
-    --image_folder ./dataset \
+    --data_path ./playground/data/LLaVA-Pretrain/blip_laion_cc_sbu_558k.json \
+    --image_folder ./playground/data/LLaVA-Pretrain/images \
     --vision_tower openai/clip-vit-large-patch14-336 \
     --tune_mm_mlp_adapter True \
     --mm_vision_select_layer -2 \
@@ -16,8 +16,8 @@ deepspeed llava/train/train_mem.py \
     --num_train_epochs 1 \
     --num_experts_per_tok 2 \
     --num_experts 4 \
-    --num_layers 4 \
-    --num_heads 4 \
+    --num_layers 2 \
+    --num_heads 2 \
     --aux_loss_coef 0.01 \
     --share_moe False \
     --cross_attention True \
