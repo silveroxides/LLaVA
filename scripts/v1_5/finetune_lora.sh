c94ee8680b850430be23adb2a98e8d21c338f9b6
20-42619-1@student.aiub.edu <20-42619-1@student.aiub.edu>
2024-08-17 22:20:38 +0600

    zero3 offload pretrain slurm

--- scripts/v1_5/finetune_lora.sh
+++ scripts/v1_5/finetune_lora.sh
@@ -2,7 +2,7 @@
 
 deepspeed llava/train/train_mem.py \
     --lora_enable True --lora_r 128 --lora_alpha 256 --mm_projector_lr 2e-5 \
-    --deepspeed ./scripts/zero3.json \
+    --deepspeed ./scripts/zero3_offload.json \
     --model_name_or_path lmsys/vicuna-13b-v1.5 \
     --version v1 \
     --data_path ./playground/data/llava_v1_5_mix665k.json \
