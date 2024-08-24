07af6f20013ad96396a72b419223eea36c44f714
20-42619-1@student.aiub.edu <20-42619-1@student.aiub.edu>
2024-08-24 10:32:05 +0600

    preparing instrcut finetuning pipeline

--- llava/model/llava_arch.py
+++ llava/model/llava_arch.py
@@ -441,11 +441,12 @@ class LlavaMetaForCausalLM(ABC):
                 cur_image_features = image_features[cur_image_idx]
                 cur_input_embeds_1 = self.get_model().embed_tokens(cur_input_ids)
                 cur_input_embeds = torch.cat([cur_input_embeds_1, cur_image_features[0:0]], dim=0)
+                print(cur_input_embeds)
                 new_input_embeds.append(cur_input_embeds)
                 new_labels.append(labels[batch_idx])
                 cur_image_idx += 1
-                text_features.append(new_input_embeds)
-                text_labels.append(new_labels)
+                text_features.append(cur_input_embeds)
+                text_labels.append(labels[batch_idx])
                 splits.append('')
                 continue
 
