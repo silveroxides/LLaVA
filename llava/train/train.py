431abeb0ae95a8e94b42ab481dadf3c536fc888a
20-42619-1@student.aiub.edu <20-42619-1@student.aiub.edu>
2024-08-24 11:18:05 +0600

    preparing instrcut finetuning pipeline

--- llava/train/train.py
+++ llava/train/train.py
@@ -212,45 +212,7 @@ def find_all_linear_names(model):
 def safe_save_model_for_hf_trainer(trainer: transformers.Trainer,
                                    output_dir: str):
     """Collects the state dict and dump to disk."""
-
-    if getattr(trainer.args, "tune_mm_mlp_adapter", False) and getattr(trainer.args, "cross_attention", False):
-        # Save Adapter and Cross Attention
-        keys_to_match = ['mm_projector', 'co_attention']
-        
-        if getattr(trainer.args, "use_im_start_end", False):
-            keys_to_match.extend(['embed_tokens', 'embed_in'])
-
-        # Extract the relevant parameters
-        weight_to_save = get_mm_adapter_state_maybe_zero_3(trainer.model.named_parameters(), keys_to_match)
-        
-        # Save model configuration
-        trainer.model.config.save_pretrained(output_dir)
-
-        # Determine where to save the weights
-        current_folder = output_dir.split('/')[-1]
-        parent_folder = os.path.dirname(output_dir)
-        
-        if trainer.args.local_rank == 0 or trainer.args.local_rank == -1:
-            if current_folder.startswith('checkpoint-'):
-                print('saving checkpoint')
-                component_folder = os.path.join(parent_folder, "components")
-                os.makedirs(component_folder, exist_ok=True)
-                for key in keys_to_match:
-                    component_weights = {k: v for k, v in weight_to_save.items() if key in k}
-                    torch.save(component_weights, os.path.join(component_folder, f'{key}_{current_folder}.bin'))
-            else:
-                for key in keys_to_match:
-                    component_weights = {k: v for k, v in weight_to_save.items() if key in k}
-                    torch.save(component_weights, os.path.join(output_dir, f'{key}.bin'))
-
-        return
-
-
-    # if trainer.deepspeed:
-    #     torch.cuda.synchronize()
-    #     trainer.save_model(output_dir)
-    #     return
-    elif getattr(trainer.args, "tune_mm_mlp_adapter", False):
+    if getattr(trainer.args, "tune_mm_mlp_adapter", False):
         # Save Adapter and Cross Attention
         keys_to_match = ['mm_projector']
         
@@ -260,6 +222,10 @@ def safe_save_model_for_hf_trainer(trainer: transformers.Trainer,
         if getattr(trainer.args, "tune_embed_tokens", False):
             keys_to_match.extend(['embed_tokens'])
 
+        if getattr(trainer.args, "cross_attention", False):
+            keys_to_match.extend(['co_attention'])
+
+        
         # Extract the relevant parameters
         weight_to_save = get_mm_adapter_state_maybe_zero_3(trainer.model.named_parameters(), keys_to_match)
         
