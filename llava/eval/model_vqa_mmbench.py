fd1167aa19cfa5cdb2c9e007811fe6231d6cc059
20-42619-1@student.aiub.edu <20-42619-1@student.aiub.edu>
2024-08-26 09:35:30 +0600

    evaluating

--- llava/eval/model_vqa_mmbench.py
+++ llava/eval/model_vqa_mmbench.py
@@ -57,7 +57,8 @@ def eval_model(args):
     model_path = os.path.expanduser(args.model_path)
     model_name = get_model_name_from_path(model_path)
     tokenizer, model, image_processor, context_len = load_pretrained_model(model_path, args.model_base, model_name)
-
+    model.config.training = False
+    
     questions = pd.read_table(os.path.expanduser(args.question_file))
     questions = get_chunk(questions, args.num_chunks, args.chunk_idx)
     answers_file = os.path.expanduser(args.answers_file)
