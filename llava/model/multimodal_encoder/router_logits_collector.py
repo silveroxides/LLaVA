95898855f5168fb78f6b7f9202bdc129fe4070aa
20-42619-1@student.aiub.edu <20-42619-1@student.aiub.edu>
2024-08-26 19:10:12 +0600

    debugging moe in encoder

--- llava/model/multimodal_encoder/router_logits_collector.py
+++ llava/model/multimodal_encoder/router_logits_collector.py
@@ -33,39 +33,4 @@ class LogitCollectorWrapper(nn.Module):
         return self.logits_buffer
 
     def clear_logits(self):
-        import torch
-import torch.nn as nn
-from collections import defaultdict
-from .moe_clip import ModifiedEncoderLayer
-
-class LogitCollectorWrapper(nn.Module):
-    def __init__(self, clip_vision_model):
-        super().__init__()
-        self.model = clip_vision_model
         self.logits_buffer = ()
-        self._hook_layers()
-
-    def _hook_layers(self):
-        for i, layer in enumerate(self.model.vision_model.encoder.layers):
-            if isinstance(layer, ModifiedEncoderLayer):
-                layer.register_forward_hook(self._create_hook(i))
-
-    def _create_hook(self, layer_idx):
-        def hook(module, input, output):
-            logits = module._last_router_logits
-            if logits is not None:
-                self.logits_buffer += (logits.detach().cpu(), )
-
-        return hook
-
-    def forward(self, *args, **kwargs):
-        return self.model(*args, **kwargs)
-
-    def get_collected_logits(self):
-        # logits_list = [torch.stack(self.logits_buffer[layer_idx]) for layer_idx in sorted(self.logits_buffer.keys())]
-        # Convert the list to a tuple and return
-        # return tuple(logits_list)
-        return self.logits_buffer
-
-    def clear_logits(self):
-        self.logits_buffer.clear()
