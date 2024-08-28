d10ffe064314cba1963284a6396553e67d67a8eb
20-42619-1@student.aiub.edu <20-42619-1@student.aiub.edu>
2024-08-28 15:27:08 +0600

    sqa benchmark debugging

--- llava/eval/model_vqa_science.py
+++ llava/eval/model_vqa_science.py
@@ -30,7 +30,11 @@ def eval_model(args):
     # Model
     disable_torch_init()
     model_path = os.path.expanduser(args.model_path)
+    print(f'Model Path: {model_path}')
+    
     model_name = get_model_name_from_path(model_path)
+    print(f'Model name: {model_name}')
+
     tokenizer, model, image_processor, context_len = load_pretrained_model(model_path, args.model_base, model_name)
     model.config.training = False
 
@@ -38,7 +42,7 @@ def eval_model(args):
     questions = get_chunk(questions, args.num_chunks, args.chunk_idx)
     answers_file = os.path.expanduser(args.answers_file)
     os.makedirs(os.path.dirname(answers_file), exist_ok=True)
-    
+
     ans_file = open(answers_file, "w")
     for i, line in enumerate(tqdm(questions)):
         idx = line["id"]
