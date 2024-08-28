90d5e807bc8a7e1ccc4948ba458f502c70d4e628
20-42619-1@student.aiub.edu <20-42619-1@student.aiub.edu>
2024-08-28 15:19:46 +0600

    sqa benchmark debugging

--- llava/eval/model_vqa_science.py
+++ llava/eval/model_vqa_science.py
@@ -38,6 +38,7 @@ def eval_model(args):
     questions = get_chunk(questions, args.num_chunks, args.chunk_idx)
     answers_file = os.path.expanduser(args.answers_file)
     os.makedirs(os.path.dirname(answers_file), exist_ok=True)
+    
     ans_file = open(answers_file, "w")
     for i, line in enumerate(tqdm(questions)):
         idx = line["id"]
